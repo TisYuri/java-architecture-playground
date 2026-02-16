2313f11594118f0adb76aa5a35d5e2a37738e2c8
TisYuri <yurimn14@outlook.com>
2026-02-16 18:00:19 -0300

    Fix Modules

---
+++ src/main/java/javaArchitecture/AdvancedDataStructuresAndSorting/maisExercicios/Ex1.java
@@ -0,0 +1,11 @@
+package javaArchitecture.AdvancedDataStructuresAndSorting.maisExercicios;
+
+//1 - Inversão de Lista:
+//Escreva um programa para inverter os elementos de um ArrayList sem usar métodos prontos do java.
+
+public class Ex1 {
+    public static void main(String[] args){
+
+
+    }
+}
